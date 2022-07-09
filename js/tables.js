3dfcd6e8a6af48909d9de352fa6829090de6b54f
Micaela Pinheiro <micaela.mews@gmail.com>
2022-07-09 17:26:41 -0300

    creante tabel script

---
+++ js/tables.js
@@ -0,0 +1,24 @@
+import { CreateElement } from "./form.js";
+const divTable = new CreateElement('container','div');
+function creenteTable (){
+    const table = divTable.appendChild(new CreateElement('table','table'));
+    table.classList.add('table-bordered','text-white','border-secondary','mt-5');
+    creanteTableHeader(table);
+    creanteTBody(table);
+}
+function creanteTableHeader(element){
+    const tableTR = element.appendChild(new CreateElement('tr','tr'));
+    const titlesTableHeader = ['Num.','Valor da Parcela', 'Amortização', 'Juros %', 'Saldo devedor'];
+
+    for(let i in titlesTableHeader){
+        const tableTH = tableTR.appendChild(new CreateElement('col','th'));
+        tableTH.classList.add('bg-secondary');
+        tableTH.textContent = titlesTableHeader[i];
+        console.log(titlesTableHeader[i]);
+    }
+}
+function creanteTBody(element){
+    const tBody = element.appendChild(new CreateElement('tbody','tbody'));
+    tBody.classList.add('bg-info');
+}
+creenteTable();
