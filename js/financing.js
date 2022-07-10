cd3e64951230efd6e88648936db0c4b9245430b9
Micaela Pinheiro <micaela.mews@gmail.com>
2022-07-10 02:38:27 -0300

    hotfix refactoring

---
+++ js/financing.js
@@ -0,0 +1,42 @@
+import { Installment } from "./installments.js";
+const tBody = document.querySelector('.tbody');
+export class Financing {
+    #interestRate;
+    #deadline;
+    #installments = [];
+
+    constructor(value, entryValue, intereseRate, deadline){
+        this.#interestRate = intereseRate;
+        this.#deadline = deadline;
+        this.#installments.push(new Installment(0,0,0,0, value - entryValue));
+    }
+    static clacInterestRate(value, interestRate){
+        return value * (interestRate / 100);
+    }
+    calcMonthlyInstallments(){
+        let balance = this.#installments[this.#installments.length-1].getBalance();
+        let deadline = this.#deadline - (this.#installments.length - 1);
+        let amortization = balance / deadline;
+        for(let i = 0; i < deadline; i++){
+            const number = this.#installments.length;
+            const interest = Financing.clacInterestRate(balance, this.#interestRate);
+            const value = interest + amortization;
+            balance -= amortization;
+            if(balance < 0 ) {balance = 0};
+            this.#installments.push(new Installment(number, value, interest, amortization, balance));
+
+        }
+    }
+    showDeadline(){
+        const installments = this.#installments.slice(1);
+        for(let installment of installments){
+            let i = 0;
+            const line = tBody.insertRow(-1);
+            for(let data of installment.getDataFormatted()){
+                const cell = line.insertCell(-1);
+                cell.textContent = data;
+            }
+            i++;
+        }
+    }
+}
